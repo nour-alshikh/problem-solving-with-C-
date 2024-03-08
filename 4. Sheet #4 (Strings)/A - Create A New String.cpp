187b36e8b0723b6b07a74c9c7d253d4e832f342b
nour-alshikh <nooralshikh30@gmail.com>
2024-03-09 00:13:02 +0200

    9/3

---
+++ 4. Sheet #4 (Strings)/A - Create A New String.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st1, st2;
+    cin >> st1 >> st2;
+
+    cout << st1.size() << " " << st2.size() << endl;
+    cout << st1 << " " << st2;
+}
