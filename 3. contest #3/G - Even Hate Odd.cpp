f0d81c0099bbe152b871a3ef693287c6b05f7384
nour-alshikh <nooralshikh30@gmail.com>
2024-03-11 14:19:10 +0200

    11/3

---
+++ 3. contest #3/G - Even Hate Odd.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--)
+    {
+        int size;
+        cin >> size;
+        int even = 0, odd = 0;
+        for (int i = 0; i < size; i++)
+        {
+            int num;
+            cin >> num;
+            if (num % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        if (even == odd)
+        {
+            cout << 0;
+        }
+        else if (size % 2 != 0)
+        {
+            cout << "-1";
+        }
+        else
+        {
+            cout << abs(even - odd) / 2;
+        }
+    }
+}
